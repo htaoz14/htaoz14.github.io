34aaa4fdae2148e2a0ad98553f1df18e15a7dc11
htaoz14 <ducanhm4v@gmail.com>
2022-05-18 15:46:50 +0700

    sege

---
+++ wed25/day_09/main.js
@@ -0,0 +1,65 @@
+console.log("hello main.js")
+// khai bao bien
+let name;
+console.log(name)
+name = " Duc anh"
+console.log(name)
+// vua khai bao bien , vua khoi tao  gia tri cho bien
+// let email = "ducanhm4v@gmai.com";
+// let number = 10;
+// let isStatus = true ;
+// kieu  du lieu nguyen thuy ( primative type)
+// underfined , string , number , boolean
+
+
+// khai bao hang so
+const PI = 3.14;
+console.log("Pi")
+email = "nfhh";
+console.log(email)
+
+let firstName = "Adu";
+let lastName = "aduu"
+
+
+console.log(typeof email);
+console.log(typeof number);
+console.log(typeof isStatus);
+//  chuoi
+let message = 'Xin chào các bạn, mình tên là \"Bùi Hiên\". Mình sinh năm 1997';
+console.log(message)
+let message1 = "Xin chào các bạn, " +
+"mình tên là \"Bùi Hiên\". " +
+"Mình sinh năm 1997";
+console.log(message1)
+//  cong chuoi
+let fullName = firstName + " "+ lastName ;
+console.log(fullName);
+
+// template String
+let address = "ha tay"
+let age = 25;
+
+console.log(`Xin chao cac ban , minh ten la guuuuu ${fullName}.minh sinh nam ${2022 - age} tuoi  , que quan o ${address}`)
+//  khong su dung
+console.log(`"xin chao cac ban , minh ten la" + fullName + "
+
+.minh sinh nam" + (2022 - age)
+" que quan o " + address`)
+// hien thi theo text nhieu dong 
+console.log ( `1
+2
+3`);
+// math
+let MathPI
+console.log(PI)
+Math.floor(Math.random() * (100 - 50 + 1 ) + 50)
+console.log(Math)
+    // Nan not a number = k bang sob + a
+
+let a = 1;
+let b = 22;
+
+console.log(b + a);
+console.log(c);
+console.log(a + name);
