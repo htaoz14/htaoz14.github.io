497896b8dbfde5ab2806b62089fd81cb29693439
htaoz14 <ducanhm4v@gmail.com>
2022-08-17 23:33:04 +0700

    update

---
+++ project/public/js/data.js
@@ -0,0 +1,28 @@
+let products =[
+    {
+        id : 1,
+        name : "Đấu sĩ baki - Phần 1",
+        price : "180,000",
+        sale : "25%",        
+        "title": "Đấu sĩ baki - phần 1",
+        description : "Baki là một cậu bé được nuôi dưỡng và đầu tư tập luyện võ thuật bởi người mẹ giàu có Akezawa Emi, với hy vọng rằng Baki có thể là một chiến binh mạnh mẽ và xứng tầm với cha cậu Hanma Yujiro.",
+        tag : "manga gốc",
+        images : [
+            
+        ],
+    },
+    {
+        id : 2,
+        name : "Đấu sĩ Baki - Phần 2",
+        price : "252000",
+        sale : "10%", 
+        description : "Baki là một cậu bé được nuôi dưỡng và đầu tư tập luyện võ thuật bởi người mẹ giàu có Akezawa Emi, với hy vọng rằng Baki có thể là một chiến binh mạnh mẽ và xứng tầm với cha cậu Hanma Yujiro.",
+        
+        tag :"manga gốc",
+        images : [
+           
+        ],
+       
+    },
+]
+    
